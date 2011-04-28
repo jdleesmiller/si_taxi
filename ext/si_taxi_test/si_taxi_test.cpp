54ec13ee5b7e5f31ea7db9dca55e73e79890bea3
enjdlm <enjdlm@36261546-225c-0410-9567-cb18639dc778>
2011-04-28 07:40:55 +0000

    profiling plan b
    git-svn-id: https://www.enm.bris.ac.uk/projects/enjdlm/si_taxi@906 36261546-225c-0410-9567-cb18639dc778

---
+++ ext/si_taxi_test/si_taxi_test.cpp
@@ -0,0 +1,18 @@
+/**
+ * These are intended mainly for profiling.
+ */
+#include <si_taxi/si_taxi.h>
+
+using namespace std;
+using namespace si_taxi;
+
+void test_bell_wong_dynamic_tp_example() {
+  //od_time = [[0, 121, 121, 121, 121], [121, 0, 181, 241, 241], [121, 241, 0, 241, 241], [121, 161, 241, 0, 161], [121, 161, 241, 241, 0]]
+  //scaled_od_demand = [[0.0, 0.005165287121782181, 0.005165287121782181, 0.005165287121782181, 0.005165287121782181], [0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0]]
+
+}
+
+int main(int *argc, char **argv) {
+  test_bell_wong_dynamic_tp_example();
+  return 0;
+}
