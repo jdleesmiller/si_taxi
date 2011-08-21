b17dfb2f40737ca9f6ba093b577ae75ee9f83fde
John Lees-Miller <jdleesmiller@gmail.com>
2011-08-21 10:25:42 +0100

    work on building model Bs from scratch

--- lib/si_taxi/mdp_model_b.rb
+++ lib/si_taxi/mdp_model_b.rb
@@ -48,6 +48,51 @@ module SiTaxi
     attr_accessor :queue, :inbound, :eta
 
     #
+    # Number of vehicles idle at each station.
+    #
+    def idle
+      k = 0
+      inbound.map{|num|
+        idle = eta[k,num].count {|r| r == 0}
+        k += num
+        idle
+      }
+    end
+
+    #
+    # Destination station of each vehicle.
+    #
+    def destin
+      inbound.map.with_index{|num,i| [i]*num}.flatten
+    end
+
+    #
+    # Basic feasibility check.
+    #
+    def feasible?
+      # cache these arrays locally to speed this up
+      idle = self.idle
+      destin = self.destin
+
+      @model.stations.all? {|i|
+        queue[i] == 0 || (idle[i] == 0 && @model.demand.rate_from(i) > 0)} &&
+        inbound.sum == @model.num_veh &&
+        @model.vehicles.all? {|k| eta[k] <= @model.max_time[destin[k]]}
+    end
+
+    #
+    # Mutate this state into the 'next' state in numerical order. Note that the
+    # resulting state may not be feasible.
+    #
+    # @return [Boolean] true iff the new state is not state zero
+    #
+    def next!
+      Utility.spin_array(queue,   @model.max_queue) ||
+      Utility.spin_array(inbound, @model.num_veh) ||
+      Utility.spin_array(eta,     @model.max_time.max)
+    end
+
+    #
     # Redefine dup to be a deep copy instead of a shallow copy.
     #
     def dup
@@ -84,41 +129,13 @@ module SiTaxi
   # elsewhere.
   #
   class MDPModelB < MDPModelBase
-    def initialize model_a
-      super(model_a.trip_time, model_a.num_veh, model_a.demand,
-            model_a.max_queue)
 
-      # note: this routine spends much of its time hashing states, so we have to
-      # be careful to avoid hashing a state more than necessary; this is why we
-      # have the "value = hash[key] ||= default" constructs
-
-      h_b = {}
-      for state_a, actions_a in model_a.to_hash
-        state_b  = MDPStateB.from_model_a_state(self, state_a)
-        h_b_actions = h_b[state_b] ||= {}
-
-        for action_a, succs_a in actions_a
-          action_b = action_from_model_a(state_a, action_a)
-          h_b_succs = h_b_actions[action_b] ||= {}
-
-          for succ_a, (pr, reward) in succs_a
-            succ_b = MDPStateB.from_model_a_state(self, succ_a)
-            pr_reward = h_b_succs[succ_b] ||= [nil, nil]
-
-            # the probability and reward for any equivalent triple should do
-            old_pr, old_reward = pr_reward
-            if old_pr
-              raise "different prob" unless old_pr == pr
-              raise "different rewards" unless old_reward == reward
-            end
-
-            pr_reward[0] = pr
-            pr_reward[1] = reward
-          end
-        end
-      end
-      @hash = h_b
+    def initialize *params
+      super(*params)
+      @hash = {}
     end
+
+    attr_reader :hash
 
     def to_hash
       @hash
@@ -135,6 +152,142 @@ module SiTaxi
         end
       end
       action_b
+    end
+
+    #
+    # Build a model B from a model A.
+    #
+    def self.new_from_model_a model_a
+      model_b = MDPModelB.new(model_a.trip_time, model_a.num_veh,
+                              model_a.demand, model_a.max_queue)
+
+      # note: this routine spends much of its time hashing states, so we have to
+      # be careful to avoid hashing a state more than necessary; this is why we
+      # have the "value = hash[key] ||= default" constructs
+
+      for state_a, actions_a in model_a.to_hash
+        state_b  = MDPStateB.from_model_a_state(model_b, state_a)
+        h_b_actions = model_b.hash[state_b] ||= {}
+
+        for action_a, succs_a in actions_a
+          action_b = model_b.action_from_model_a(state_a, action_a)
+          h_b_succs = h_b_actions[action_b] ||= {}
+
+          for succ_a, (pr, reward) in succs_a
+            succ_b = MDPStateB.from_model_a_state(model_b, succ_a)
+            pr_reward = h_b_succs[succ_b] ||= [nil, nil]
+
+            # the probability and reward for any equivalent triple should do
+            old_pr, old_reward = pr_reward
+            if old_pr
+              raise "different prob" unless old_pr == pr
+              raise "different rewards" unless old_reward == reward
+            end
+
+            pr_reward[0] = pr
+            pr_reward[1] = reward
+          end
+        end
+      end
+
+      model_b
+    end
+
+    def self.new_from_scratch trip_time, num_veh, demand, max_queue
+      model = MDPModelB.new(trip_time, num_veh, demand, max_queue)
+      h = model.hash
+
+      # enumerate feasible states
+      state = MDPStateB.new(model)
+      begin
+        h[state.dup] = {} if state.feasible?
+      end while state.next!
+
+      # enumerate actions for each state, then possible successor states
+      for state, state_actions in h
+        for action in Utility::cartesian_product(*state.idle.map{|sum|
+          Utility::integer_partitions(sum, model.num_stations)})
+          state_actions[action] = model.transitions(state, action)
+        end
+      end
+
+      model
+    end
+
+    #
+    # Generate all possible successor states, their rewards, and their
+    # transition probabilities.
+    #
+    def transitions state, action
+      # TODO not done yet
+      {}
+#      # find vehicles already idle (eta 0) or about to become idle (eta 1),
+#      # but ignore those that are moving away due to the action
+#      available = stations.map {|i| vehicles.select {|k|
+#        state.destin[k] == i && state.eta[k] <= 1 && action[k] == i}}
+#      #puts "available: #{available.inspect}"
+#
+#      # for each station, the basic relationship is:
+#      #   new_queue = max(0, queue + new_pax - (idle + landing - leaving))
+#      # because there can't be both idle vehicles and waiting passengers;
+#      # we want all values of new_pax that make new_queue <= max_queue
+#      max_new_pax = stations.map {|i|
+#        max_queue - state.queue[i] + available[i].count}
+#      new_pax = [0]*num_stations
+#      begin
+#        # add new pax to waiting pax (note that this may exceed max_queue)
+#        # need to know how many pax we can serve now (rest must queue)
+#        pax_temp   = stations.map {|i| state.queue[i] + new_pax[i]}
+#        pax_served = stations.map {|i| [available[i].count, pax_temp[i]].min}
+#        #puts "state: #{state.inspect}"
+#        #puts "pax_temp: #{pax_temp}"
+#        #puts "pax_served: #{pax_served}"
+#
+#        # update queue states and vehicles due to actions
+#        new_state = state.dup
+#        new_state_pr = 1.0
+#        stations.each do |i|
+#          new_state.queue[i] = pax_temp[i] - pax_served[i]
+#          new_state.destin = action.dup
+#
+#          pr_i = demand.poisson_origin_pmf(i, new_pax[i])
+#          pr_i += demand.poisson_origin_cdf_complement(i, new_pax[i]) if
+#            new_state.queue[i] == max_queue
+#          raise "bug: pr_i > 1" if pr_i > 1
+#          new_state_pr *= pr_i
+#        end
+#
+#        # the above generates states with non-zero queues at stations with zero
+#        # arrival rates, which would cause us to generate infeasible states
+#        if new_state_pr > 0
+#          # need to know destinations for any pax we're serving
+#          journey_product = stations.map {|i|
+#            journeys_from_i = stations.map {|j| [i, j] if i != j}.compact
+#            Utility.cartesian_product(*[journeys_from_i]*pax_served[i])}.compact
+#          #puts "new_state: #{new_state.inspect}"
+#          #puts "journey_product:\n#{journey_product.inspect}"
+#          if journey_product.empty?
+#            # no passengers served; just yield new_state
+#            new_state.set_eta_from(state)
+#            yield new_state, new_state_pr
+#          else
+#            Utility.cartesian_product(*journey_product).each do |journeys|
+#              available_for_pax = available.map{|ai| ai.dup}
+#              pax_state = new_state.dup
+#              pax_state_pr = new_state_pr
+#              #puts "journeys: #{journeys.inspect}"
+#              journeys.flatten(1).each do |i, j|
+#                pax_state.destin[available_for_pax[i].shift] = j
+#                pax_state_pr *= demand.at(i, j) / demand.rate_from(i)
+#                #puts "pax_state: #{pax_state.inspect}"
+#              end
+#              pax_state.set_eta_from(state)
+#              #puts "pax_state: #{pax_state.inspect}"
+#              yield pax_state, pax_state_pr
+#            end
+#          end
+#        end
+#      end while Utility.spin_array(new_pax, max_new_pax)
     end
   end
 end
--- test/mdp_model_b_test.rb
+++ test/mdp_model_b_test.rb
@@ -24,7 +24,7 @@ class MDPModelBTest < Test::Unit::TestCase
       @m_a  = MDPModelA.new([[0,1],[1,0]], 1, od, 1)
       @hm_a = FiniteMDP::HashModel.new(@m_a.to_hash)
       @tm_a = FiniteMDP::TableModel.from_model(@hm_a)
-      @m_b  = MDPModelB.new(@m_a)
+      @m_b  = MDPModelB.new_from_model_a(@m_a)
       @hm_b = FiniteMDP::HashModel.new(@m_b.to_hash)
       @tm_b = FiniteMDP::TableModel.from_model(@hm_b)
     end
@@ -69,7 +69,7 @@ class MDPModelBTest < Test::Unit::TestCase
       @m_a = MDPModelA.new([[0,1],[1,0]], 1, od, 2)
       @hm_a = FiniteMDP::HashModel.new(@m_a.to_hash)
       @tm_a = FiniteMDP::TableModel.from_model(@hm_a)
-      @m_b  = MDPModelB.new(@m_a)
+      @m_b  = MDPModelB.new_from_model_a(@m_a)
       @hm_b = FiniteMDP::HashModel.new(@m_b.to_hash)
       @tm_b = FiniteMDP::TableModel.from_model(@hm_b)
     end
@@ -94,7 +94,7 @@ class MDPModelBTest < Test::Unit::TestCase
       @m_a = MDPModelA.new([[0,1],[1,0]], 2, od, 1)
       @hm_a = FiniteMDP::HashModel.new(@m_a.to_hash)
       @tm_a = FiniteMDP::TableModel.from_model(@hm_a)
-      @m_b  = MDPModelB.new(@m_a)
+      @m_b  = MDPModelB.new_from_model_a(@m_a)
       @hm_b = FiniteMDP::HashModel.new(@m_b.to_hash)
       @tm_b = FiniteMDP::TableModel.from_model(@hm_b)
     end
@@ -140,6 +140,92 @@ class MDPModelBTest < Test::Unit::TestCase
       assert_equal 25, solver.policy.size
     end
   end
-end
 
+  context "building model from scratch" do
+    context "two station ring with one vehicle; max_queue=1" do
+      setup do
+        od = ODMatrixWrapper.new([[0,0.2],[0.3,0]])
+        @m_b  = MDPModelB.new_from_scratch([[0,1],[1,0]], 1, od, 1)
+        #@hm_b = FiniteMDP::HashModel.new(@m_b.to_hash)
+        #@tm_b = FiniteMDP::TableModel.from_model(@hm_b)
+      end
+
+      should "" do
+        s = MDPStateB.new(@m_b)
+
+        assert_equal [0,0], s.inbound
+        assert_equal [0,0], s.idle
+
+        s.inbound[0] = 1
+        assert_equal [1,0], s.inbound
+        assert_equal [1,0], s.idle
+      end
+
+      #should "have valid transition probabilities" do
+      #  @m_b.check_transition_probabilities_sum
+      #  @tm_b.check_transition_probabilities_sum
+      #end
+
+      #should "have 12 feasible states" do
+      #  assert_equal [[0, 0, 0, 1, 0], # idle at 1
+      #                [0, 0, 0, 1, 1], # going to 1
+      #                [0, 0, 1, 0, 0], # idle at 0
+      #                [0, 0, 1, 0, 1], # going to 0
+      #                [0, 1, 0, 1, 1],
+      #                [0, 1, 1, 0, 0],
+      #                [0, 1, 1, 0, 1],
+      #                [1, 0, 0, 1, 0],
+      #                [1, 0, 0, 1, 1],
+      #                [1, 0, 1, 0, 1],
+      #                [1, 1, 0, 1, 1],
+      #                [1, 1, 1, 0, 1]], @hm_b.states.uniq.sort.map{|s| s.to_a}
+      #end
+
+      #should "cover action space" do
+      #  assert_equal [[[0, 0], [0, 0]],  # do nothing 
+      #                [[0, 1], [0, 0]],  # move from 0 to 1 
+      #                [[0, 0], [1, 0]]], # move from 1 to 0
+      #    @hm_b.states.map {|s| @hm_b.actions(s)}.flatten(1).uniq!
+      #end
+    end
+
+    context "two station ring with two vehicles; max_queue=1" do
+      setup do
+        od = ODMatrixWrapper.new([[0,0.2],[0.3,0]])
+        @m_b  = MDPModelB.new_from_scratch([[0,1],[1,0]], 2, od, 1)
+        p @m_b.to_hash
+      end
+
+      should "count idle vehicles in state" do
+        s = MDPStateB.new(@m_b)
+
+        # default state is not feasible: no inbound vehicles
+        assert_equal [0,0], s.inbound
+        assert_equal [0,0], s.eta
+        assert_equal [0,0], s.idle
+        assert_equal [], s.destin
+        assert !s.feasible?
+
+        # make inbound feasible; should get one idle vehicles at each station
+        s.inbound[0] = 1
+        s.inbound[1] = 1
+        assert s.feasible?
+        assert_equal [1,1], s.idle
+
+        # set one vehicle to move to 1; the other remains idle at 0
+        s.eta[1] = 1
+        assert_equal [1,0], s.idle
+
+        # try the other way around
+        s.eta[0] = 1
+        s.eta[1] = 0
+        assert_equal [0,1], s.idle
+
+        # and finally try with no idle vehicles
+        s.eta[1] = 1
+        assert_equal [0,0], s.idle
+      end
+    end
+  end
+end
 
